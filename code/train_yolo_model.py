2aaa47e6eb309aa8a90962acc8fa8c343696540b
unknown <crvelasquez19@gmail.com>
2025-07-01 10:42:48 -0400

    trained model 1st time

--- code/train_yolo_model.py
+++ code/train_yolo_model.py
@@ -8,16 +8,16 @@ model = YOLO("yolo11n.yaml")
 # Train the model on the COCO8 dataset for 100 epochs
 train_results = model.train(
     data="config.yaml",  # Path to dataset configuration file
-    epochs=30,  # Number of training epochs
+    epochs=100,  # Number of training epochs
     imgsz=640,  # Image size for training
 )
 
-# # Evaluate the model's performance on the validation set
-# metrics = model.val()
+# Evaluate the model's performance on the validation set
+metrics = model.val()
 
-# # Perform object detection on an image
-# results = model("path/to/image.jpg")  # Predict on an image
-# results[0].show()  # Display results
+# Perform object detection on an image
+results = model("path/to/image.jpg")  # Predict on an image
+results[0].show()  # Display results
 
-# # Export the model to ONNX format for deployment
-# path = model.export(format="onnx")  # Returns the path to the exported model
+# Export the model to ONNX format for deployment
+path = model.export(format="onnx")  # Returns the path to the exported model
